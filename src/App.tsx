ff892a08ffd4f61c0d9519d22ff29ec90e9843a9
Andy Nadvorny <andynadvorny@gmail.com>
2023-03-11 13:22:08 -0300

    01: styling and linting setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-export function App() {
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
+export function App() {
   return (
-    <h1>hello world</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>hello world</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
